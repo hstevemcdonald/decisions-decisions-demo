4caa3d8eb28f64da1142608d5d008e7a8cc797a4
Steve McDonald <hstevemcdonald@gmail.com>
2018-09-09 10:38:36 -0700

    Refactor seed. Add 'spin' effect.

--- seed/seed.js
+++ seed/seed.js
@@ -1,22 +1,43 @@
-var PouchDB = require('pouchdb');
-var db = new PouchDB('http://localhost:4000/choices');
-const read = require('read-file');
-const log = require('ololog');
+var PouchDB = require("pouchdb");
+var db = new PouchDB("http://localhost:4000/choices");
+const read = require("read-file");
+const log = require("ololog");
+const uuid = require("uuid");
+
+let currentSet = [];
+
+const load = () => {
+  return db.allDocs({ include_docs: true }).then(docs => {
+    currentSet = docs.rows.map(row => row.doc.choice);
+    log(currentSet, currentSet.length);
+  });
+};
 
 const choices = read.sync("./seed/list.txt");
 
-let inc=0;
-choices.toString().split("\n").map(choice => {
-    if (choice.trim()) {
+load().then(() => {
+  let inc = currentSet.length;
+  choices
+    .toString()
+    .split("\n")
+    .map(choice => {
+      choice = choice.trim();
+      if (choice && !currentSet.includes(choice)) {
         inc++;
         try {
-            db.put({"_id": inc.toString(), choice: choice.trim()})
+          let _id = uuid();
+          console.log({ _id, choice });
+          log("inserted ", _id, choice);
+          return db.put({ _id: _id.toString(), choice }).catch(e => {
+            log(e);
+            process.exit(1);
+          });
         } catch (e) {
-            log(e)
-            process.exit(0)
+          log(e);
+          process.exit(1);
         }
-        
-    }
-})
+      }
+    });
 
-console.log("Inserted " + inc + "choices.")
+  console.log("Inserted " + (inc - currentSet.length) + " choices.");
+});
--- src/App.js
+++ src/App.js
@@ -1,60 +1,72 @@
-import React, { Component } from 'react';
-import log from 'ololog';
-import PouchDB  from 'pouchdb';
+import React, { Component } from "react";
+import log from "ololog";
+import PouchDB from "pouchdb";
 
-import Button from './components/button';
-import Choice from './components/choice';
+import Button from "./components/button";
+import Choice from "./components/choice";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.handlePick = this.handlePick.bind(this);
-    this.db = null;
     this.state = {
-      choices:[],
-      choice: 0
-    }
+      choices: [],
+      choice: 0,
+      choosing: false
+    };
   }
 
   componentDidMount() {
     let _this = this;
     try {
-      this.db = new PouchDB('http://localhost:4000/choices');
+      const db = new PouchDB("http://localhost:4000/choices");
+      db.allDocs({ include_docs: true }).then(docs => {
+        this.setState(() => ({
+          choices: docs.rows.map(row => row.doc.choice)
+        }));
+      });
     } catch (e) {
-      log.bright.red(e)
+      log.bright.red(e);
+      process.exit(0);
     }
-    
   }
+
   render() {
-    console.log(this.state)
     return (
       <div>
-        {this.state.choice && <div className="App">
-          <Choice choice={this.state.choices[this.state.choice]} />
-          <br/>
-          </div>}
-          <Button handlePick={this.handlePick} />  
+        {this.state.choice && (
+          <div className="App">
+            <Choice choice={this.state.choices[this.state.choice]} />
+            <br />
+          </div>
+        )}
+        {!this.state.choosing && <Button handlePick={this.handlePick} />}
       </div>
-    )
+    );
   }
 
-  async handlePick() {
+  handlePick() {
     let _this = this;
-    let docSet = await  this.db.allDocs({include_docs: true})
-    _this.setState(() => ({choices:docSet.rows.map(row => row.doc.choice)}))
-
-    let increment = .011
+    let timeout = null;
+    let increment = 1.0000000000001;
+    this.setState(() => ({ choosing: true }));
     function spin(time = 0) {
-      if (time<2) {
-          increment**=2
-          setTimeout(() => {
-            _this.setState(() => ({choice: Math.floor(Math.random()*docSet.rows.length)}))
-            time+=increment;
-            spin(time);
-          }, increment)
+      if (time < 200) {
+        increment **= 1.25;
+        timeout = setTimeout(() => {
+          _this.setState(() => ({
+            choice: Math.floor(Math.random() * _this.state.choices.length)
+          }));
+          time += increment;
+          console.log(time);
+          spin(time);
+        }, time);
+        return;
       }
+      _this.setState(() => ({ choosing: false }));
+      clearTimeout(timeout);
     }
-    spin()
+    spin();
   }
 }
 
